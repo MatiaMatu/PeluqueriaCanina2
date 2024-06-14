3108b94b7fda7d80f2b5713fbe195b4a88f5e36d
MattySm <139090532+MattySm@users.noreply.github.com>
2024-06-13 21:43:26 -0400

    home,navbar todo menos cerrar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var $: any; // Declarar la variable $ para usar jQuery
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {}
+
+  
+
 }
