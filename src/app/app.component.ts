f246cd33735f00e6e53aabcae933bb9352cc81f7
MattySm <139090532+MattySm@users.noreply.github.com>
2024-06-14 16:02:10 -0400

    home completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
+import { FirebaseService } from 'src/app/services/firebase.service';
 declare var $: any; // Declarar la variable $ para usar jQuery
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
+  
 })
 export class AppComponent {
+  firebaseSvc = inject(FirebaseService);
   constructor() {}
+
+  signOut() {
+    this.firebaseSvc.signOut();
+   }
+  
 
   
 
